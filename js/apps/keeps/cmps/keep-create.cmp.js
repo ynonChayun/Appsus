0d4d1169f145402408447482760034dddeb28bc6
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-06-30 10:30:43 +0300

    fixed

--- js/apps/keeps/cmps/keep-create.cmp.js
+++ js/apps/keeps/cmps/keep-create.cmp.js
@@ -20,6 +20,7 @@ export default {
                     </button>    
             </div>
             <button @click="createNote">Add note</button>
+        </div>
     </section>
     `,
     data() {
--- js/main-nav.cmp.js
+++ js/main-nav.cmp.js
@@ -1,21 +1,21 @@
 export default {
     template: `
             <nav class="main-nav">
-                <router-link to="/mail" @click.native="$emit('clicked')" class="nav-link ">
-                    <img src="imgs/gmail.png">
-                        Email
-                </router-link>
-                <router-link to="/keep" @click.native="$emit('clicked')" class="nav-link ">
-                    <img src="imgs/keeps.png" >
-                        Keep
-                </router-link>
                 <router-link to="/" @click.native="$emit('clicked')" class="nav-link ">
                     <img src="imgs/book-stack.png">
                         Books
                 </router-link>
+                <router-link to="/mail" @click.native="$emit('clicked')" class="nav-link ">
+                    <img src="imgs/gmail.png">
+                        Email
+                </router-link>
                 <router-link to="/" @click.native="$emit('clicked')" class="nav-link ">
                     <img src="imgs/house.png">
                         Home
+                </router-link>
+                <router-link to="/keep" @click.native="$emit('clicked')" class="nav-link ">
+                    <img src="imgs/keeps.png" >
+                        Keep
                 </router-link>
             </nav>
 `,
