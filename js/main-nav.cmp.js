92288d0fe5a193ac82e959ac4cbed867fc3c196a
ynonchayun <ynonchayun@gmail.com>
2022-07-01 11:56:55 +0300

    upload

--- js/main-nav.cmp.js
+++ js/main-nav.cmp.js
@@ -1,13 +1,13 @@
 export default {
     template: `
             <nav class="main-nav">
-                <router-link to="/" @click.native="$emit('clicked')" class="nav-link ">
+                <router-link to="/book" @click.native="$emit('clicked')" class="nav-link ">
                     
                 <img src="imgs/book-stack.png">
                 </router-link>
                 <router-link to="/mail" @click.native="$emit('clicked')" class="nav-link ">
                     <img src="imgs/gmail.png">
-
+                
                 </router-link>
                 <router-link to="/" @click.native="$emit('clicked')" class="nav-link ">
                     <img src="imgs/house.png">
--- js/router.js
+++ js/router.js
@@ -1,6 +1,7 @@
 import homePage from './home-page.cmp.js'
 import keepPage from './apps/keeps/views/keepPage.js'
 import mailPage from './apps/email/views/email-app.cmp.js'
+// import bookPage from './apps/books/views/book-app.cmp.js'
 
 const routes = [
     {
